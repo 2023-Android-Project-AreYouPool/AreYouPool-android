1b90f81bbdbc90a17f486c1c21570c7d146357b3
ezi-s-u <ljsgoo0617@gmail.com>
2023-10-19 19:14:55 +0900

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "are_you_pool_android"
+include(":app")
